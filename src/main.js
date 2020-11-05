5f44e98b3f544fac8afec8dc339ec05df0c9a9ae
Krzesimir Modelski <k.modelski96@gmail.com>
2020-11-05 23:03:56 +0100

    added sorting and improved validation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-document.getElementById("button").addEventListener("click", calcFunction)
+document.getElementById("button").addEventListener("click", calcEquation)
 
-
-function calcFunction() {
+function calcEquation() {
     let equation = document.getElementById("equation").value.toString().replace(/\s+/g, "")
 
-    if(validateInput(equation)) {
-        let array = equation.split("+").join(",").split("-").join(",");
+    if (validateInput(equation)) {
+        equation = equation.replace(/-/g, ",-").replace(/\+/g, ",+")
+        let array = addSameElements(equation.split(",").filter(item => item !== ""));
 
+        let a = array[0].replace("x^2", "");
+        let b = array[1].replace("x", "");
+        let c = array[2];
+        let delta = calcDelta(a,b,c);
+        console.log(delta);
     }
 }
 
@@ -24,7 +29,32 @@ function validateInput(input) {
     }
     return true;
 }
+
 function showWrongInput() {
     console.log("wrong input")
     /*TODO SHOW WRONG INPUT ALERT*/
 }
+
+function addSameElements(array) {
+   let tempArray = [0,0,0];
+   let returnValue = [];
+
+    array.forEach(item => {
+        if(item.includes("x^2") ) {
+            tempArray[0] += parseFloat(item.replace("x^2", ""));
+        }else if(item.includes("x") ) {
+            tempArray[1] += parseFloat(item.replace("x^2", ""));
+        } else {
+            tempArray[2] += parseFloat(item);
+        }
+    })
+    returnValue.push(tempArray[0].toString().concat("x^2"));
+    returnValue.push(tempArray[1].toString().concat("x"));
+    returnValue.push(tempArray[2].toString());
+    return returnValue;
+}
+
+function calcDelta(a,b,c) {
+    return Math.pow(b,2) - (4 * a * c);
+}
+
